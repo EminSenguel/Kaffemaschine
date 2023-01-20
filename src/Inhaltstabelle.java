a350cadd64f2b9b7e9388a8e6c9ad99c2661764b
christoph.blenke <christophblenke@web.de>
2023-01-20 14:54:18 +0100

    added games

--- src/Inhaltstabelle.java
+++ src/Inhaltstabelle.java
@@ -4,29 +4,4 @@ import static java.lang.System.out;
 
 public class Inhaltstabelle {
 
-    public static void main(String[] args) {
-
-        Scanner z = new Scanner(System.in);
-
-
-        out.println("Bitte wähle eines der Folgenden Produkte: " + "\n" + "Drücke '1' - Cappucino " + "\n" +
-                "Drücke '1' - Kaffe schwarz ");
-
-        int selection = z.nextInt();
-
-        switch (selection) {
-            case 1:
-                // get balance
-                out.println("Zahlenduell");
-
-                break;
-            case 2:
-                out.println("Zahlenraten");
-
-
-
-                break;
-
-
-        }
 }
