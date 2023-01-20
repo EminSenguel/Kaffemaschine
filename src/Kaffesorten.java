df01d0a1656dcc295acfd10148db792ff201c3fa
EminS <wi22236@lehre.dhbw-stuttgart.de>
2023-01-20 10:10:30 +0100

    Inital Commit

---
+++ src/Kaffesorten.java
@@ -0,0 +1,15 @@
+public class Kaffesorten {
+
+    public static void KaffeeSchwarz(){
+        Stärke();
+
+    }
+
+    public static void Americano(){
+
+}
+    public static void Stärke(){
+
+}
+
+}
